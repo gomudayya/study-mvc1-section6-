a691a3fa457ef1fd202aba17f4996594cf803283
GeonWoo Park <gomudayya@gmail.com>
2023-05-08 19:30:33 +0900

    spring mvc 기본 기능 코드

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,22 @@
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class MappingController {
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET) // @GetMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+
+}
